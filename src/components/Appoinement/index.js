d9afe0338664c42d69b62da615b8c914aa365cb7
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-16 17:58:16 -0600

    appointment component initial implementation

---
+++ src/components/Appoinement/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import 'components/Appoinement/styles.scss';
+
+const Appointment = props => {
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+export default Appointment;
