aeb5f83da35146e031c1b40ece6aa5dda7c87bc0
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-16 11:40:03 -0600

    InterviewerListItem component implementation

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import "components/InterviewerListItem.scss";
+
+const InterviewerListItem = props => {
+  const listClasses = classNames(
+    "interviewers__item",
+    { "interviewers__item--selected": props.selected }
+  );
+
+  const imgClasses = classNames(
+    "interviewers__item-image",
+    { "interviewers__item-image--selected": props.selected }
+  );
+
+  return (
+    <li 
+      className={listClasses}
+      key={props.id}
+      onClick={props.setInterviewer}
+    >
+      <img
+        className={imgClasses}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
