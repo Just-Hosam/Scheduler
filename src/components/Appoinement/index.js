23d2d272fc6acae4657dd7188df108cecc13fd47
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-16 20:49:31 -0600

    Appointment component implementation

--- src/components/Appoinement/index.js
+++ src/components/Appoinement/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import 'components/Appoinement/styles.scss';
+// Import the Header, Show and Empty components into Appointment/index.js.
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 const Appointment = props => {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
   );
 };
 
