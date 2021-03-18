1c4fef6b218265d70dc6d4d2c16cefab16df4509
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-18 15:56:23 -0600

    mode changing functionality implementation

--- src/components/Appoinement/index.js
+++ src/components/Appoinement/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import useVisualMode from '../../hooks/useVisualMode';
 
 import 'components/Appoinement/styles.scss';
 // Import the Header, Show and Empty components into Appointment/index.js.
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = props => {
+  const { mode, transition, back } = useVisualMode((props.interview) ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE &&
+        <Form 
+          interviewers={[]}
+          onCancel={() => back()}  
+        />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 };
