c223f184adc428e2c35e9bfdda72bd60d366f32d
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-20 16:26:37 -0600

    saving functionality implementation

--- src/components/Appoinement/index.js
+++ src/components/Appoinement/index.js
@@ -2,34 +2,50 @@ import React from 'react';
 import useVisualMode from '../../hooks/useVisualMode';
 
 import 'components/Appoinement/styles.scss';
-// Import the Header, Show and Empty components into Appointment/index.js.
+// Components import
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 
+// Form modes
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 const Appointment = props => {
   const { mode, transition, back } = useVisualMode((props.interview) ? SHOW : EMPTY);
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW));
+  }
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE &&
         <Form 
-          interviewers={[]}
-          onCancel={() => back()}  
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+          onSave={save}
         />}
-      {mode === SHOW && (
+      {mode === SAVING && <Status message='Saving!' /> }
+      {mode === SHOW && 
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      )}
+      }
     </article>
   );
 };
