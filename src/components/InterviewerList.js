a841aeee5d55eb2286e59b8aa3f66e1d231786bb
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-22 15:37:35 -0600

    check if interviewers is array with proptypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 
@@ -23,6 +24,10 @@ const InterviewerList = props => {
       </ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
 
 export default InterviewerList;
