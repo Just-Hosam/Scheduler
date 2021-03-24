60352b98a740233b441c860bd6d7e3dc97b158d4
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-24 16:19:11 -0600

    Delete route implementaion into mock axios

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -89,4 +89,13 @@ export default {
 			});
 		}
 	}),
+	delete: jest.fn((url) => {
+		if (url === '/api/appointments/2') {
+			return Promise.resolve({
+				status: 204,
+				statusText: 'No Content',
+				data: fixtures.appointments['2'],
+			});
+		}
+	}),
 };
